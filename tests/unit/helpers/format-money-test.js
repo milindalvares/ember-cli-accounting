96fddc7f75c323997f9021dd305964cd11887a76
Miguel Camba <miguel.camba@gmail.com>
2015-08-14 08:29:43 +0100

    Remove support for old versions of Ember.

--- tests/unit/helpers/format-money-test.js
+++ tests/unit/helpers/format-money-test.js
@@ -1,16 +1,15 @@
 import { test, module } from 'qunit';
 import formatMoney from 'accounting/format-money';
-import callHelper from '../../helpers/call-helper';
 
 module('Helpers: format-money');
 
 test("formats money the with the default configuration", function(assert) {
-  assert.equal(callHelper(formatMoney, 123.4567, {}), "$123.46");
+  assert.equal(formatMoney([123.4567], {}), "$123.46");
 });
 
 test("formats money the with options passed to the handlebars template", function(assert) {
-  assert.equal(callHelper(formatMoney, 123.4567, { precision: 2 }), "$123.46");
-  assert.equal(callHelper(formatMoney, 123.4567, { precision: 2, decimal: ","}), "$123,46");
-  assert.equal(callHelper(formatMoney, 1234.567, { precision: 2, decimal: ",", thousand: "::"}), "$1::234,57");
-  assert.equal(callHelper(formatMoney, 1234.567, { precision: 2, symbol: "€", format: "%v %s"}), "1,234.57 €");
+  assert.equal(formatMoney([123.4567], { precision: 2 }), "$123.46");
+  assert.equal(formatMoney([123.4567], { precision: 2, decimal: ","}), "$123,46");
+  assert.equal(formatMoney([1234.567], { precision: 2, decimal: ",", thousand: "::"}), "$1::234,57");
+  assert.equal(formatMoney([1234.567], { precision: 2, symbol: "€", format: "%v %s"}), "1,234.57 €");
 });
